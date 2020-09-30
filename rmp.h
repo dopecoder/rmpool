3139adf275853acc6ebdee28e7859f73d099ef58
Nanda Velugoti <nandshnu@gmail.com>
2020-09-30 16:11:43 -0500

    Initial RMP interface

--- rmp.h
+++ rmp.h
@@ -1,19 +1,31 @@
 #ifdef RMP_H
 #define RMP_H
 
-#include <netinet.h>
+namespace rmp {
 
-typedef struct {
-	int id;
-	struct sockaddr_in sock_addr;
-} rmp_host;
+typedef int rmp_handle;
 
-void rmp_connect();
-void rmp_alloc();
-void rmp_free();
-void rmp_write();
-void rmp_read();
+namespace guest {
+// Guest
+void rmp_connect(struct config);
+char* rmp_alloc(int n_pages); // receives a rmp_handle from host
+void rmp_free(rmp_handle hd);
 
-void rmp_host_setup();
+// private guest
+void pagefault_handler(void* args);
+void rmp_write(rmp_handle hd, int offset, void* data);
+void* rmp_read(rmp_handle hd, int offset);
+}
 
+namespace host {
+// Host
+void rmp_host_setup(struct config);
+void rmp_host_alloc(int n_pages); // sends a rmp_handle to guest
+void rmp_host_free(rmp_handle hd);
+
+// private host
+void rmp_host_write(rmp_handle hd, int offset, void* data);
+void* rmp_host_read(rmp_handle hd, int offset);
+}
+}
 #endif
